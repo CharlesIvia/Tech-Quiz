5938f807f8d0a6538577a669809db7ce66c9be90
Charles Ivia <iviacharles@gmail.com>
2020-06-06 14:46:53 +0300

    Fetched dom elements and assigned variables

---
+++ script.js
@@ -0,0 +1,6 @@
+//Fetch DOM elements
+
+const question = document.querySelector(".question");
+const options = document.querySelector(".options");
+const score = document.querySelector(".score");
+const message = document.querySelector(".message");
